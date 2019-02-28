da4a1ddb7dd6e7614d6777ec20fb99371578fa21
jsb18181 <james.mackenzie.2018@uni.strath.ac.uk>
2019-02-28 14:38:34 +0000

    Test

--- app/src/main/java/com/assignment/alt_shift_cs991/MainActivity.java
+++ app/src/main/java/com/assignment/alt_shift_cs991/MainActivity.java
@@ -1,13 +1,27 @@
 package com.assignment.alt_shift_cs991;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fm;
+    private String tag;
+    private ShiftSwapDialogue swap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        swap = new ShiftSwapDialogue();
+        FragmentManager fm = getSupportFragmentManager();
+
+        swap.show(fm, tag);
     }
+
 }
---
+++ app/src/main/java/com/assignment/alt_shift_cs991/ShiftSwapDialogue.java
@@ -0,0 +1,31 @@
+package com.assignment.alt_shift_cs991;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class ShiftSwapDialogue extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.swap_message)
+                .setPositiveButton(R.string.accept_button, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(getContext(),"Your shift has been swapped!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton(R.string.reject_button, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the proposed swap
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+}
