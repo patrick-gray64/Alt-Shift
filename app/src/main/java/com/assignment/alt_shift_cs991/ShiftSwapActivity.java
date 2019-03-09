128cfa2cf24decd55a21f527e08aac95053b24c0
rjmcrae <43537412+rjmcrae@users.noreply.github.com>
2019-03-09 19:26:46 +0000

    Added new button to swap to return to home screen
    Merge branch 'develop' of https://gitlab.cis.strath.ac.uk/jsb18181/cs991-group-project into develop
    
    # Conflicts:
    #	.idea/misc.xml
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/assignment/alt_shift_cs991/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/assignment/alt_shift_cs991/ShiftSwapActivity.java
+++ app/src/main/java/com/assignment/alt_shift_cs991/ShiftSwapActivity.java
@@ -3,8 +3,14 @@ package com.assignment.alt_shift_cs991;
 import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.assignment.alt_shift_cs991.databinding.SwapLayoutBinding;
@@ -17,6 +23,7 @@ public class ShiftSwapActivity extends Toolbar_activity {
     ShiftModel shiftModel;
     private ObjectAnimator shiftWorkerCardAnimation;
     private ObjectAnimator userCardAnimation;
+    private ImageButton swapButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,7 @@ public class ShiftSwapActivity extends Toolbar_activity {
         SwapLayoutBinding shiftSwapLayoutBinding = DataBindingUtil.setContentView(this, R.layout.swap_layout);
         shiftSwapLayoutBinding.setShiftModel(shiftModel);
         initToolbar();
+        swapButton =  findViewById(R.id.shift_button);
     }
 
 
@@ -50,11 +58,32 @@ public class ShiftSwapActivity extends Toolbar_activity {
             @Override
             public void onAnimationStart(Animator animation) {
                 v.setEnabled(false);
+                swapButton.setClickable(false);
             }
 
             @Override
             public void onAnimationEnd(Animator animation) {
                 v.setEnabled(true);
+                Button confirmButton = new Button(v.getContext());
+                confirmButton.setBackgroundResource(R.drawable.button_layout);
+                confirmButton.setText("Confirm Swap");
+                confirmButton.setTextColor(Color.parseColor("#ffffff"));
+                confirmButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), CalendarActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                RelativeLayout layout = (RelativeLayout)findViewById(R.id.rlayout);
+                RelativeLayout.LayoutParams laypram = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                laypram.addRule(RelativeLayout.BELOW, R.id.cardSwapHolder);
+                laypram.addRule(RelativeLayout.ALIGN_END, R.id.cardSwapHolder);
+                laypram.setMargins(10, 10, 30, 10);
+                layout.addView(confirmButton, laypram);
+
+                //(new Handler()).postDelayed(this::returnToHome, 500);
+
             }
 
             @Override
